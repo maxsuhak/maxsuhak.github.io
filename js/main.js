7564bd90838f82277817a2d739d158c7f7fd53b1
Maksym Suhak <maxsuhak@gmail.com>
2020-03-17 14:00:58 +0200

    maxsuhak.github.io initial

---
+++ js/main.js
@@ -0,0 +1,35 @@
+(function($) {
+  "use strict"
+
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function (event) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash)
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
+      if (target.length) {
+        event.preventDefault()
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        },
+        1000,
+        function () {
+          var $target = $(target)
+          $target.focus()
+          if ($target.is(":focus")) {
+            return false
+          } else {
+            $target.attr('tabindex', '-1')
+            $target.focus()
+          }
+        })
+      }
+    }
+  })
+
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide')
+  })
+
+  $('body').scrollspy({
+    target: '#sideNav'
+  })
+})(jQuery)
